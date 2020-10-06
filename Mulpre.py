c722a8f7387b6a967cd3c92cb60d6bd92bf30477
Ravi <marsstrikers@gmail.com>
2020-10-06 13:20:24 +0545

    MulPre Continuous added

--- Mulpre.py
+++ Mulpre.py
@@ -1,3 +1,5 @@
+
+# import time
 op = 'y'
 while(op.upper()=='Y'):
     a = input("\nNum:")
@@ -10,4 +12,24 @@ while(op.upper()=='Y'):
         a = ans
         print(ans, end="\n")
     print("Multiplicative Persistence = ",i, end='\n')
-    op=input("Continue?(Y/N)")
+    op = input("Continue?(Y/N)")
+# start_time=time.time()
+# num=0
+# i = 0
+# while(num<10000000000000000 or i==4):
+#     a = num
+#     print(num,end="\n")
+#     i=0
+#     while(int(a) % 10 !=int(a)):
+#         ans = 1
+#         for x in str(a):
+#             ans = ans * int(x)
+#         a = ans
+#         i=i+1
+#         # print(ans, end="\n")
+#     num = num+1
+#
+#     print("Multiplicative Persistence = ",i, end='\n')
+#     if(i==9):
+#         break
+# print(time.time()-start_time)
